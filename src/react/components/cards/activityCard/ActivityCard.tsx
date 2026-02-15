7dde14b30542c89646db2add8a221a96c1d09344
vladimircuriel <voco0001@ce.pucmm.edu.do>
2026-02-15 16:41:51 -0400

    fix: adjust CSS classes for improved layout and styling in ActivityCard component

--- src/react/components/cards/activityCard/ActivityCard.tsx
+++ src/react/components/cards/activityCard/ActivityCard.tsx
@@ -9,7 +9,7 @@ export default function ActivityCard({ activity }: ActivityProps) {
     <div className="flex items-center justify-center overflow-y-visible">
       <div className="flex drop-shadow-xl flex-col items-center min-h-40 w-64 lg:min-h-[350px] lg:min-w-[390px] rounded-3xl bg-accent-light">
         <img
-          className="w-56 h-36 m-4 lg:w-[320px] lg:h-44 rounded-2xl"
+          className="w-56 h-36 m-4 lg:w-[320px] lg:h-44 rounded-2xl object-cover"
           src={activity.coverImage}
           alt={activity.name}
           loading="lazy"
@@ -23,7 +23,7 @@ export default function ActivityCard({ activity }: ActivityProps) {
             target="_blank"
             rel="noopener noreferrer"
             href={activity.href}
-            className="w-[90%] p-2 mt-9 mb-4 lg:mb-0 text-xl font-medium text-center transition-all rounded-2xl bg-primary hover:scale-105 hover:bg-primary/90"
+            className="w-[90%] p-2 mt-auto mb-4 lg:mb-0 text-xl font-medium text-center transition-all rounded-2xl bg-primary hover:scale-105 hover:bg-primary/90"
           >
             <span className="text-white">Ver más</span>
           </a>
